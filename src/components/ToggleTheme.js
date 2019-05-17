c7ef602022dc5bbc775743cbb69c33b1b1a47fad
AntergosX1 <nanodesign21@gmail.com>
2019-05-16 22:29:29 -0300

    toggleTheme button hook refactor

--- src/components/ToggleTheme.js
+++ src/components/ToggleTheme.js
@@ -1,37 +1,31 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import ToggleBtn from "./ToggleBtn";
 import { MoonSVG, SunSVG } from "./Icons";
 
-class ToggleTheme extends Component {
-  constructor() {
-    super();
-    this.state = {
-      lightTheme: false
-    };
-  }
+const ToggleTheme = () => {
+  const [theme, setTheme] = useState(false);
 
-  toggle() {
-    const lightTheme = this.state.lightTheme ? "light" : "dark";
-    document.documentElement.setAttribute("data-theme", lightTheme);
-    this.setState({ lightTheme: !this.state.lightTheme });
-  }
-
-  componentDidMount() {
-    this.toggle();
-  }
-
-  render() {
-    return (
-      <ToggleBtn
-        cClass="btn btn-theme"
-        IconT={MoonSVG}
-        IconF={SunSVG}
-        value={this.state.lightTheme}
-        toggle={() => this.toggle()}
-        disabled={false}
-      />
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      "data-theme",
+      theme ? "light" : "dark"
     );
-  }
-}
+  });
+
+  const toggle = () => {
+    setTheme(!theme);
+  };
+
+  return (
+    <ToggleBtn
+      cClass="btn btn-theme"
+      IconT={MoonSVG}
+      IconF={SunSVG}
+      value={theme}
+      toggle={toggle}
+      disabled={false}
+    />
+  );
+};
 
 export default ToggleTheme;
