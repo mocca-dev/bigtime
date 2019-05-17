65fe0285def8e9a0f807a8d6fee2cee7782d2772
AntergosX1 <nanodesign21@gmail.com>
2019-05-17 00:40:45 -0300

    full app hook refator

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import TimeDisplay from "./components/TimeDisplay";
 import ProgressBar from "./components/ProgressBar";
 import AudioUpload from "./components/AudioUpload";
@@ -8,197 +8,171 @@ import SettingsBtn from "./components/SettingsBtn";
 import SettingsBar from "./components/SettingsBar";
 import { PlaySVG, PauseSVG, StopSVG } from "./components/Icons";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      minutes: "00",
-      seconds: "00",
-      playing: false,
-      lightTheme: true,
-      songName: null,
-      autoPlay: true,
-      showToaster: false,
-      showSettings: false,
-      bucle: true
-    };
+const App = ({ appServiceWorker }) => {
+  const [minutes, setMinutes] = useState("00");
+  const [seconds, setSeconds] = useState("00");
+  const [playing, setPlaying] = useState(false);
+  // const [theme, setTheme] = useState(true);
+  const [songName, setSongName] = useState(null);
+  const [showToaster, setShowToaster] = useState(false);
+  const [showSettings, setShowSettings] = useState(false);
+  const [bucle, setBucle] = useState(true);
+  const [progressWidth, setProgressWidth] = useState(0);
 
-    this.playerRef = React.createRef;
-    this.audioInputRef = React.createRef;
-  }
+  const playerRef = useRef(null);
+  const audioInputRef = useRef(null);
 
-  componentDidMount() {
+  useEffect(() => {
     document.documentElement.setAttribute("data-theme", "dark");
+  });
 
-    this.props.appServiceWorker.onUpdateFound(() =>
-      this.setState({ showToaster: true })
-    );
+  useEffect(() => {
+    appServiceWorker.onUpdateFound(() => setShowToaster(true));
+  });
 
-    if (this.playerRef) {
-      this.audioInputRef.onchange = fileInput => {
+  useEffect(() => {
+    if (playerRef.current && audioInputRef.current) {
+      audioInputRef.current.onchange = fileInput => {
         const files = fileInput.target;
         const file = URL.createObjectURL(files.files[0]);
-        this.setState({ songName: files.files[0].name });
-        this.playerRef.src = file;
-        this.togglePlay();
+        setSongName(files.files[0].name);
+        playerRef.current.src = file;
+        togglePlay();
       };
     }
-  }
+  });
 
-  convertOffsetXToCurrentTime(e) {
-    this.playerRef.currentTime =
-      this.playerRef &&
-      (e.nativeEvent.offsetX * this.playerRef.duration) / window.innerWidth;
-  }
+  useEffect(() => {
+    setProgressWidth(convertCurrentTimeToWidth());
+  });
 
-  convertCurrentTimeToWidth() {
-    if (this.state) {
-      const player = this.playerRef;
-      const duration = player && player.duration;
-      const currentTime = player && player.currentTime;
-      const calc = (100 * currentTime) / duration;
-      return !isNaN(calc) ? Math.floor(calc) : 0;
-    }
-  }
+  const convertOffsetXToCurrentTime = e => {
+    playerRef.current.currentTime =
+      playerRef &&
+      (e.nativeEvent.offsetX * playerRef.current.duration) / window.innerWidth;
+  };
 
-  calcTime() {
-    const player = this.playerRef;
+  const convertCurrentTimeToWidth = () => {
+    const player = playerRef.current;
+    const duration = player && player.duration;
+    const currentTime = player && player.currentTime;
+    const calc = (100 * currentTime) / duration;
+    return !isNaN(calc) ? Math.floor(calc) : 0;
+  };
+
+  const calcTime = () => {
+    const player = playerRef.current;
     const totalSeconds = player.currentTime;
     const minutes = "0" + Math.floor(totalSeconds / 60);
-    this.setState({ minutes });
+    setMinutes(minutes);
+    let seconds;
+
     if (totalSeconds < 60) {
-      const seconds =
+      seconds =
         totalSeconds < 10
           ? "0" + Math.floor(totalSeconds)
           : Math.floor(totalSeconds);
-      this.setState({ seconds });
     } else {
-      const seconds =
+      seconds =
         totalSeconds % 60 < 10
           ? "0" + Math.floor(totalSeconds % 60)
           : "" + Math.floor(totalSeconds % 60);
-      this.setState({ seconds });
     }
-  }
+    setSeconds(seconds.toString());
+  };
 
-  isFinished() {
-    const player = this.playerRef;
-    const { duration, currentTime } = player;
-    return duration === currentTime;
-  }
-
-  restartWhenFinished() {
-    if (this.isFinished()) {
-      this.stop(this.playerRef);
-      this.playerRef.play();
-      this.setState({ playing: true });
+  const stop = player => {
+    if (player) {
+      player.pause();
+      player.currentTime = 0;
+      togglePlay();
     }
-  }
+  };
 
-  stopWhenFinished() {
-    if (this.isFinished()) {
-      this.stop(this.playerRef);
-    }
-  }
-
-  togglePlay() {
-    const { playing } = this.state;
-    const player = this.playerRef;
+  const togglePlay = () => {
+    const player = playerRef.current;
     if (playing) {
       player.pause();
     } else {
       player.play();
     }
-    this.calcTime();
-    this.setState({ playing: !playing });
-  }
+    calcTime();
+    setPlaying(!playing);
+  };
 
-  stop(player) {
-    if (player) {
-      player.pause();
-      player.currentTime = 0;
-      this.togglePlay();
+  const isFinished = () => {
+    const { duration, currentTime } = playerRef.current;
+    return duration === currentTime;
+  };
+
+  const restartWhenFinished = () => {
+    if (isFinished()) {
+      stop(playerRef.current);
+      playerRef.current.play();
+      setPlaying(true);
     }
-  }
+  };
 
-  render() {
-    let {
-      minutes,
-      seconds,
-      playing,
-      showToaster,
-      songName,
-      bucle,
-      showSettings
-    } = this.state;
+  const stopWhenFinished = () => {
+    if (isFinished()) {
+      stop(playerRef.current);
+    }
+  };
 
-    const progressWidth = this.convertCurrentTimeToWidth();
-
-    return (
-      <div className="App">
-        {showToaster && (
-          <Toaster
-            content="Nueva actualizacion!"
-            close={() => this.setState({ showToaster: false })}
-            ok={() => window.location.reload()}
-          />
+  return (
+    <div className="App">
+      {showToaster && (
+        <Toaster
+          content="Nueva actualizacion!"
+          close={() => setShowToaster(false)}
+          ok={() => window.location.reload()}
+        />
+      )}
+      <ProgressBar
+        progressWidth={progressWidth}
+        onMouseDown={e => songName && convertOffsetXToCurrentTime(e)}
+      />
+      <TimeDisplay minutes={minutes} seconds={seconds} />
+      <audio
+        ref={playerRef}
+        onTimeUpdate={() => {
+          calcTime();
+          if (bucle) {
+            restartWhenFinished();
+          } else {
+            stopWhenFinished();
+          }
+        }}
+      />
+      <AudioUpload ref={audioInputRef} labelTxt={songName} />
+      <div className="bottom">
+        {showSettings && (
+          <SettingsBar bucle={bucle} toggleBucle={() => setBucle(!bucle)} />
         )}
-        <ProgressBar
-          progressWidth={progressWidth}
-          onMouseDown={e => songName && this.convertOffsetXToCurrentTime(e)}
-        />
-        <TimeDisplay minutes={minutes} seconds={seconds} />
-        <audio
-          ref={ref => {
-            this.playerRef = ref;
-          }}
-          onTimeUpdate={() => {
-            this.calcTime();
-            if (bucle) {
-              this.restartWhenFinished();
-            } else {
-              this.stopWhenFinished();
-            }
-          }}
-        />
-        <AudioUpload
-          setRef={ref => (this.audioInputRef = ref)}
-          labelTxt={songName}
-        />
-        <div className="bottom">
-          {this.state.showSettings && (
-            <SettingsBar
-              bucle={bucle}
-              toggleBucle={() => this.setState({ bucle: !bucle })}
-            />
-          )}
-          <div className="bottom-bar">
-            <div className="left-btns">
-              <SettingsBtn
-                onClick={() => this.setState({ showSettings: !showSettings })}
-              />
-              <button
-                className="btn btn-stop"
-                onClick={() => {
-                  this.stop(this.playerRef);
-                }}
-              >
-                <StopSVG />
-              </button>
-            </div>
-            <ToggleBtn
-              cClass="btn btn-play"
-              IconT={PauseSVG}
-              IconF={PlaySVG}
-              value={playing}
-              toggle={() => this.togglePlay()}
-              disabled={!songName}
-            />
+        <div className="bottom-bar">
+          <div className="left-btns">
+            <SettingsBtn onClick={() => setShowSettings(!showSettings)} />
+            <button
+              className="btn btn-stop"
+              onClick={() => {
+                stop(playerRef.current);
+              }}
+            >
+              <StopSVG />
+            </button>
           </div>
+          <ToggleBtn
+            cClass="btn btn-play"
+            IconT={PauseSVG}
+            IconF={PlaySVG}
+            value={playing}
+            toggle={() => togglePlay()}
+            disabled={!songName}
+          />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
--- src/components/AudioUpload.js
+++ src/components/AudioUpload.js
@@ -1,26 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const AudioUpload = ({ setRef, labelTxt }) => {
-  return (
-    <React.Fragment>
-      <input
-        className="input-audio"
-        type="file"
-        ref={ref => setRef(ref)}
-        name="audio-file"
-        id="audio-file"
-        accpet="audio/*"
-      />
-      <label htmlFor="audio-file">
-        {labelTxt ? labelTxt : "Seleccionar tema..."}
-      </label>
-    </React.Fragment>
-  );
-};
+const AudioUpload = React.forwardRef(({ labelTxt }, ref) => (
+  <React.Fragment>
+    <input
+      className="input-audio"
+      type="file"
+      ref={ref}
+      name="audio-file"
+      id="audio-file"
+      accpet="audio/*"
+    />
+    <label htmlFor="audio-file">
+      {labelTxt ? labelTxt : "Seleccionar tema..."}
+    </label>
+  </React.Fragment>
+));
 
 AudioUpload.propTypes = {
-  setRef: PropTypes.func.isRequired,
   labelTxt: PropTypes.string
 };
 
