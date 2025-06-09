c32775aa7b5cd75c09747b000834ea01d4a9f774
Damelanote2501 <nicolastonelli1406@gmail.com>
2025-06-09 00:17:14 -0300

    Updates

---
+++ precache-manifest.a83b0be20f1400427abe47c3822a896b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "07afb419d6b1117d7450",
+    "url": "/bigtime/static/css/main.98072806.chunk.css"
+  },
+  {
+    "revision": "07afb419d6b1117d7450",
+    "url": "/bigtime/static/js/main.26d44288.chunk.js"
+  },
+  {
+    "revision": "6c917be1fcdf0cf1e028",
+    "url": "/bigtime/static/js/runtime~main.e1df5096.js"
+  },
+  {
+    "revision": "27f50a08f1bdac330153",
+    "url": "/bigtime/static/js/2.4fd8f4c8.chunk.js"
+  },
+  {
+    "revision": "90a2213be516f8dfeb362b9a68181e5a",
+    "url": "/bigtime/index.html"
+  }
+];
